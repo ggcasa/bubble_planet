97863f65b09f793a174655a8e3ffcf870ff04e73
ggcasa <casandru40@gmail.com>
2026-06-01 22:24:48 +0100

    additinals: replace generate_csv.sh with executable ../bin/generate.csv  and import  "math/rand"

--- additional/generate_csv/main.go
+++ additional/generate_csv/main.go
@@ -14,16 +14,14 @@ func random(min, max int) int {
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
 
 	// Valori implicite
-	var lungimeString int64 = 17 // Lungimea din exemplul tău (JQGNJGTLLWZZGNMCM)
+	var lungimeString int64 = 17
 	numeFisier := "output.csv"
-	var numarLinii int64 = 5000 // Numărul implicit de înregistrări
+	var numarLinii int64 = 5000
 
 	arguments := os.Args
 
-	// Citim argumentele din terminal sequentially
 	if len(arguments) > 1 {
 		lungimeString, _ = strconv.ParseInt(arguments[1], 10, 64)
 	}
@@ -36,7 +34,6 @@ func main() {
 
 	fmt.Printf("Pornire generare: %d linii, ID lungime %d -> %s\n", numarLinii, lungimeString, numeFisier)
 
-	// 1. Creăm fișierul pe disc
 	fisier, err := os.Create(numeFisier)
 	if err != nil {
 		fmt.Printf("Eroare la crearea fișierului: %v\n", err)
@@ -47,7 +44,6 @@ func main() {
 	writer := csv.NewWriter(fisier)
 	defer writer.Flush()
 
-	// Scrie Antetul (Header)
 	header := []string{"ID_Bula", "Data_Creare", "Status"}
 	if err := writer.Write(header); err != nil {
 		fmt.Printf("Eroare la scrierea antetului: %v\n", err)
@@ -57,11 +53,8 @@ func main() {
 	startChar := byte('A')
 	timestampAcum := time.Now().Format("2006-01-02 15:04:05")
 
-	// 2. Bucla principală care generează numărul de linii cerut
 	var linie int64
 	for linie = 0; linie < numarLinii; linie++ {
-
-		// Generăm ID-ul unic pentru linia curentă
 		idAleatoriu := ""
 		var i int64 = 1
 		for {
@@ -73,7 +66,6 @@ func main() {
 			i++
 		}
 
-		// Scriem rândul direct în buffer-ul CSV
 		dateRow := []string{idAleatoriu, timestampAcum, "activ"}
 		if err := writer.Write(dateRow); err != nil {
 			fmt.Printf("Eroare la scrierea liniei %d: %v\n", linie, err)
